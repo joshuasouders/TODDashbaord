7daced41b4b94fdc50724a4a04e495779d1125d5
Josh Souders <rem87062@gmail.com>
2014-12-05 15:17:07 -0500

    removed old test controller file

--- frontend/js/controllers/controller.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('StationMapDashboard', ['leaflet-directive']);
-
-app.controller('MapDataController', function ($scope, $http) {
-	$http.jsonp('http://localhost:8080/rest/getMapData?callback=JSON_CALLBACK').success(function(data, status, headers, config) {
-	    $scope.stations = data.features;
-	});
-});
-
