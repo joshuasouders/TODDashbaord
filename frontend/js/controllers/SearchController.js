fd5a38fd22119ab535901cb663146e9cd8cc5e60
Joshua Souders <souders.josh@gmail.com>
2015-07-20 16:40:11 -0400

    search now populates by the first rail line type

--- frontend/js/controllers/SearchController.js
+++ frontend/js/controllers/SearchController.js
@@ -2,7 +2,29 @@ app.controller('SearchController', ['$scope', 'stationService', 'activeStationSe
 	$scope.selectedStation = {};
 
 	$scope.$on('stationServiceReady', function() {
-		$scope.stationJSON = stationService.getJSON().sort(function(a, b){
+		var stations = stationService.getJSON();
+		var additionalStations = [];
+		for(var i = 0; i < stations.length; i++){
+			if(typeof stations[i]["rail_lines_served"] != "undefined"){
+				var splitStation = stations[i]["rail_lines_served"].split("; ");
+				if(splitStation.length > 1){
+					stations[i]["rail_lines_served"] = splitStation[0];
+				}
+				//This code is a step in the right direction toward adding stations to each rail type if a station serves multiple rail lines
+				//Currently it only populates the first rail line, which is good enough for now. There's an Angular bug getting in the way.
+				/*for(var x = 1; x < splitStation.length; x++){
+					var alteredStation = stations[i];
+					alteredStation["rail_lines_served"] = splitStation[x];
+					additionalStations.push(alteredStation);
+				}*/
+			}
+		}
+		//stations = stations.concat(additionalStations);
+		$scope.stationJSON = [];
+		$scope.stationJSON = $scope.stationJSON.concat(stations);
+		console.log($scope.stationJSON);
+
+		$scope.stationJSON.sort(function(a, b){
 		    return a['station_name'] < b['station_name'] ? -1 : a['station_name'] > b['station_name'] ? 1 : 0;
 		});;
 	});
